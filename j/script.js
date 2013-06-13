f965cfe5d0beabe9195bf62a248ab4ae104b585e
Kae Verens <kae@kvsites.ie>
2013-06-13 23:30:22 +0100

    use Lawnchair, instead of directly accessing local storage

--- j/script.js
+++ j/script.js
@@ -6,20 +6,7 @@ function onDeviceReady() {
 			},
 			true
 		);
-	$.indexedDB(
-			'kvwebmeNewsReader',
-			{
-				'schema':{
-					'1': function(versionTransaction) {
-						versionTransaction
-							.createObjectStore( 'blog', { 'keyPath':'id' })
-							.createIndex('pdate')
-							.createIndex('id', {'unique': true});
-					}
-				}
-			}
-		)
-		.done(init);
+	init();
 }
 // { setZeroTimeout (very fast replacement for setTimeout)
 var setZeroTimeout=function(a){if(a.postMessage){var b=[],c="asc0tmot",d=function(a){b.push(a),postMessage(c,"*")},e=function(d){if(d.source==a&&d.data==c){d.stopPropagation&&d.stopPropagation();if(b.length)try{b.shift()()}catch(e){setTimeout(function(a){return function(){throw a.stack||a}}(e),0)}b.length&&postMessage(c,"*")}};if(a.addEventListener)return addEventListener("message",e,!0),d;if(a.attachEvent)return attachEvent("onmessage",e),d}return setTimeout}(window);
@@ -71,31 +58,72 @@ function init() {
 function blogRefresh() {
 	var $wrapper=$('#blog-articles');
 	var stories=[];
-	$.indexedDB('kvwebmeNewsReader').objectStore('blog')
-		.each(function(result) {
-			var row=result.value;
-			var img='';
-			if (row.image) {
-				img='<img src="'+config.site+row.image+'" style="width:100%"/>';
+	Lawnchair(function() {
+		this.get('blog', function(results) {
+			if (results===null) {
+				return;
 			}
-			var d=row.pdate
-				.replace(/....-0*([0-9]*)-0*([0-9]*) (..:..).*/, '$1|$2|$3');
-			var bits=d.split('|');
-			d=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][bits[0]-1]+' '+bits[1]+' '+bits[2];
-			var html='<tr data-id="'+row.id+'"><td style="width:20%" rowspan="2">'
-				+img+'</td>'
-				+'<td colspan="2" class="title">'+row.title+'</td></tr>'
-				+'<tr data-id="'+row.id+'"><td class="tags">'+row.tags+'</td>'
-				+'<td class="date">'+d+'</td></tr>'
-				+'<tr><td colspan="3" class="seperator">&nbsp;</td></tr>';
-			stories.push(html);
-		})
-		.done(function() {
+			$.each(results.stories, function(k, row) {
+				var img='';
+				if (row.image) {
+					img='<img src="'+config.site+row.image+'" style="width:100%"/>';
+				}
+				var d=row.pdate
+					.replace(/....-0*([0-9]*)-0*([0-9]*) (..:..).*/, '$1|$2|$3');
+				var bits=d.split('|');
+				d=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][bits[0]-1]+' '+bits[1]+' '+bits[2];
+				var html='<tr data-id="'+row.id+'" id="blog-header-'+row.id+'">'
+					+'<td style="width:20%" rowspan="2">'
+					+img+'</td>'
+					+'<td colspan="2" class="title">'+row.title+'</td></tr>'
+					+'<tr data-id="'+row.id+'"><td class="tags">'+row.tags+'</td>'
+					+'<td class="date">'+d+'</td></tr>'
+					+'<tr data-id="'+row.id+'"><td colspan="3" class="blog-body" id="blog-body-'+row.id+'"/></tr>'
+					+'<tr><td colspan="3" class="seperator">&nbsp;</td></tr>';
+				stories.push(html);
+			});
 			$wrapper.empty().append(
 				'<table style="width:100%">'+stories.join('')+'</table>'
 			);
+			$wrapper.find('tr').click(function() {
+				var id=$(this).data('id');
+				blogBodyShow(id);
+			});
 		});
+	});
 	$('body').scrollTop(0);
+}
+function blogBodyShow(id) {
+	Lawnchair(function() {
+		this.get('blog-body-'+id, function(result) {
+			console.log(result);
+			if (result===null) {
+				$('#blog-body-'+id).html('loading...');
+				$.post(
+					config.site+'/a/p=blog/f=postGet',
+					{'id':id},
+					function(ret) {
+						Lawnchair(function() {
+							this.save(
+								{
+									'key':'blog-body-'+id,
+									'body':ret.body
+								},
+								function() {
+									blogBodyShow(id);
+								}
+							);
+						});
+						console.log(ret);
+					}
+				);
+				return;
+			}
+			$('.blog-body').empty();
+			$('#blog-body-'+id).html(result.body);
+			$('body').scrollTop($('#blog-header-'+id).offset().top);
+		});
+	});
 }
 function blogDownload() {
 	clearTimeout(window.blogDownloadTimer);
@@ -103,32 +131,13 @@ function blogDownload() {
 		window.blogDownloadTimer=setTimeout(blogDownload, 1000);
 		return;
 	}
-	window.blogDownloadTimer=setTimeout(blogDownload, 60000);
+	window.blogDownloadTimer=setTimeout(blogDownload, 600000);
 	$.post(
 		config.site+'/a/p=blog/f=getRecent',
 		function(ret) {
-			var newStories=0;
-			$.indexedDB('kvwebmeNewsReader')
-				.transaction('blog')
-				.then(
-					function() {
-						console.log(newStories, 'new stories');
-						if (newStories) {
-							setZeroTimeout(blogRefresh);
-						}
-					},
-					function() {
-					},
-					function(transaction) {
-						var blogs=transaction.objectStore('blog');
-						$.each(ret, function(k, v) {
-							v.id=+v.id;
-							blogs.add(v).ondone=function() {
-								newStories++;
-							}
-						});
-					}
-				);
+			Lawnchair(function() {
+				this.save({'key':'blog','stories':ret}, blogRefresh);
+			});
 		}
 	);
 }
